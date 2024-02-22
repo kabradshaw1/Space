0f86b63eb44e474ac374f0b11fe5fc59dcc6c9a6
Kyle <itllbok82@gmail.com>
2024-02-21 23:50:33 -0500

    first file

--- core/src/com/mygdx/game/learning/Demo.java
+++ core/src/com/mygdx/game/learning/Demo.java
@@ -3,5 +3,7 @@ package com.mygdx.game.learning;
 public class Demo {
   public static void main(String[] args) {
     
+    String message = "Hello Java";
+    System.out.println("Hello Java");
   }
 }
