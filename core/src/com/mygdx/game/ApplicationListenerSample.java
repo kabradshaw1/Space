b0d0f31706fa14a1260d8ae342696c398c44255b
Kyle <itllbok82@gmail.com>
2024-02-22 13:16:43 -0500

    end lession 41

--- core/src/com/mygdx/game/ApplicationListenerSample.java
+++ core/src/com/mygdx/game/ApplicationListenerSample.java
@@ -1,31 +1,53 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Logger;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class ApplicationListenerSample implements ApplicationListener{
+
+	private static final Logger log = new Logger(ApplicationListenerSample.class.getName(), Logger.DEBUG);
+
+	private boolean renderInterrupted = true;
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		// Used to initialize game and load resources
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		log.debug("create()");
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void resize(int width, int height) {
+		// Used to handle a new screen size
+		log.debug("resize() width = " + width + " height = " + height);
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void render() {
+		// Used to render the game elements called 60 times per second
+		throw new UnsupportedOperationException("Unimplemented method 'render'");
 	}
+
+	@Override
+	public void pause() {
+		// used to save game state when it loses focus, which doesn't involv the actual game play being paused unless the develp wants it to puase
+		if(renderInterrupted) {
+			log.debug("render()");
+			renderInterrupted = false;
+		}
+	}
+
+	@Override
+	public void resume() {
+		// used to handle the game coming back from being pused and restores game state
+
+	}
+
+	@Override
+	public void dispose() {
+		// used t free resources and cleanup
+
+	}
+
 }
--- desktop/src/com/mygdx/game/DesktopLauncherApplicationListenerSample.java
+++ desktop/src/com/mygdx/game/DesktopLauncherApplicationListenerSample.java
@@ -2,14 +2,14 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+// import com.mygdx.game.MyGdxGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
-public class DesktopLauncher {
+public class DesktopLauncherApplicationListenerSample {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("SpaceGame");
-		new Lwjgl3Application(new MyGdxGame(), config);
+		new Lwjgl3Application(new ApplicationListenerSample(), config);
 	}
 }
