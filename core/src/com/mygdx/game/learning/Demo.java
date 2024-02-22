63fe29c1349e8fe3924a97536f19f6cd38707098
Kyle <itllbok82@gmail.com>
2024-02-22 10:58:29 -0500

    another slow tutorial

--- core/src/com/mygdx/game/learning/Demo.java
+++ core/src/com/mygdx/game/learning/Demo.java
@@ -1,9 +1,13 @@
 package com.mygdx.game.learning;
 
 public class Demo {
+
+  static int lives = 3;
+  static String playerName = "John";
+  static boolean gameOver;  // false by default
   public static void main(String[] args) {
     
     String message = "Hello Java";
-    System.out.println("Hello Java");
+    System.out.println(message);
   }
 }
